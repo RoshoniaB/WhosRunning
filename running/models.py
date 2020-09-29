46bd5ef0b9242f74e1f7d89277a21dacb1d46250
Gaspy <mpgasbarro@gmail.com>
2020-09-29 16:18:12 -0400

    have model populating on localhost and have created superuser

--- running/models.py
+++ running/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Running(models.Model):
     name = models.CharField(max_length=100)
-    picture_url = models.TexField()
+    picture_url = models.TextField()
     party_affiliation = models.TextField()
     campaign_website = models.TextField()
     social = models.TextField()
